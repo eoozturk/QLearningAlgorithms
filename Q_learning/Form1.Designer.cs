9faa9e1c00a62c67306c1c64f7c43b216eb6c30f
Emre Ozan Öztürk <eo.ozturk@hotmail.com>
2017-08-29 22:29:28 +0300

    Oluşturma

---
+++ Q_learning/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Yazlab_Q_learning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBaslangic = new System.Windows.Forms.TextBox();
+            this.Txthedef = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textIterasyon = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Komşuluk Listesini Okuyun";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBaslangic
+            // 
+            this.textBaslangic.Location = new System.Drawing.Point(131, 224);
+            this.textBaslangic.Name = "textBaslangic";
+            this.textBaslangic.Size = new System.Drawing.Size(41, 20);
+            this.textBaslangic.TabIndex = 5;
+            this.textBaslangic.Text = "0";
+            // 
+            // Txthedef
+            // 
+            this.Txthedef.Location = new System.Drawing.Point(131, 250);
+            this.Txthedef.Name = "Txthedef";
+            this.Txthedef.Size = new System.Drawing.Size(41, 20);
+            this.Txthedef.TabIndex = 6;
+            this.Txthedef.Text = "0";
+            this.Txthedef.TextChanged += new System.EventHandler(this.Txthedef_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Başlangıç Odacık : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Hedef Odacık : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 279);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "İterasyon Sayısı : ";
+            // 
+            // textIterasyon
+            // 
+            this.textIterasyon.Location = new System.Drawing.Point(131, 276);
+            this.textIterasyon.Name = "textIterasyon";
+            this.textIterasyon.Size = new System.Drawing.Size(41, 20);
+            this.textIterasyon.TabIndex = 9;
+            this.textIterasyon.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 43);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 160);
+            this.listBox1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "R MATRİSİ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(238, 43);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 160);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(486, 43);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(220, 160);
+            this.textBox5.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(195, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Uygula";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(563, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Q MATRİSİ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(336, 239);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 31);
+            this.textBox2.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(375, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "EN KISA YOL";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(359, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 30);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "txt olarak kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button4.CausesValidation = false;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(566, 242);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 33);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Labirenti Çizdir";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 318);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textIterasyon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Txthedef);
+            this.Controls.Add(this.textBaslangic);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Ana Ekran";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBaslangic;
+        private System.Windows.Forms.TextBox Txthedef;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textIterasyon;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Q_learning/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace Yazlab_Q_learning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            try
+            {
+                openFileDialog1.FileName = "Okunacak Dosyayı Seçiniz.";
+                openFileDialog1.Filter = "Yazı Dosyaları (*txt)|*.txt";
+                openFileDialog1.ShowDialog();
+
+                StreamReader okumaislemi = new StreamReader(openFileDialog1.FileName);
+                string satir=okumaislemi.ReadLine();
+
+                for (i=0;satir!=null;i++) 
+                {
+                    listBox1.Items.Add(satir);
+                    satir=okumaislemi.ReadLine();
+                }
+                
+                okumaislemi.Close();
+
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message);
+            }
+          
+        }
+
+
+        ArrayList yoldizi=new ArrayList();
+        int timer = 1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int row = listBox1.Items.Count;
+            int[,] R = new int[row, row];
+            double[,] Q = new double[row, row];
+            for (int k = 0; k < row; k++)
+            {
+                for (int l = 0; l < row; l++)
+                {
+                    R[k, l] = -1;
+                    Q[k, l] = 0.0;
+                }
+            }
+
+            for (int s = 0; s < row; s++)
+            {
+                string[] parcala = listBox1.Items[s].ToString().Split(',');
+                for (int j = 0; j < parcala.Length; j++)
+                {
+                    if (Convert.ToInt32(parcala[j]) == Convert.ToInt32(Txthedef.Text))
+                        R[s, Convert.ToInt32(parcala[j])] = 100;                          
+                    else
+                        R[s, Convert.ToInt32(parcala[j])] = 0;
+                }
+            }
+            R[Convert.ToInt32(Txthedef.Text),Convert.ToInt32(Txthedef.Text)] = 100;
+            
+            // R MATRİSİ
+            for (int k = 0; k < row; k++)
+            {
+                for (int l = 0; l < row; l++)
+                {
+                    textBox1.AppendText(R[k, l] + "  ");
+                }
+                textBox1.AppendText(Environment.NewLine);
+            }
+
+            int iterasyon= Convert.ToInt32(textIterasyon.Text);
+            int baslangic= Convert.ToInt32(textBaslangic.Text);
+            int durum;
+            int g;
+            ArrayList c = new ArrayList();
+            int x;
+            Random rnd=new Random();
+            int act = 0;
+            int hedef = Convert.ToInt32(Txthedef.Text);
+           for (int k = 0; k < 3000; k++)
+            {
+                durum = rnd.Next(row);
+              while(durum!=hedef)
+              {
+                  c.Clear();
+                  for ( g = 0; g < row ; g++)
+			        {
+                         if (R[durum, g] != -1)
+                            {
+                                c.Add(g); 
+                            }
+                    }
+                     x = rnd.Next(c.Count);
+                     act =Convert.ToInt32(c[x]);
+                    // Q maximum
+                     double maxx = -1;
+                         for (g = 0; g < row; g++)
+                         {
+                             if (R[act, g] != -1)
+                             {
+                                 if (maxx < Q[act, g])
+                                 {
+                                     maxx = Q[act, g];
+                                 }
+                             }
+                         }
+                    
+                  // Q maximum
+                  Q[durum, act] = R[durum,act]+ 0.8 * maxx;
+                  durum=act;
+          }
+      }
+
+            // Q MATRİSİ YAZDIRMA
+           for (int k = 0; k < row; k++)
+            {
+                for (int l = 0; l < row; l++)
+                {
+                    textBox5.AppendText(Math.Round(Q[k, l],1) + " - ");
+                }
+                textBox5.AppendText(Environment.NewLine);
+            }
+            durum=baslangic;
+            double yolmax=0.0;
+            int nextpath=0;
+            textBox2.AppendText(baslangic + " - ");
+            yoldizi.Add(baslangic);
+            while (durum != hedef)
+            {
+                for (g = 0; g < row; g++)
+                {
+                    if (Q[durum, g] > yolmax)
+                        {
+                            yolmax = Q[durum, g];
+                            nextpath = g;
+                           
+                        }
+                    
+                }
+                yoldizi.Add(nextpath);
+                textBox2.AppendText(nextpath + " - " );
+                durum = nextpath;
+                
+            }
+
+        }
+
+        private void Txthedef_TextChanged(object sender, EventArgs e)
+        {
+            int s = listBox1.Items.Count;
+            try
+            {
+                if (s <= Convert.ToInt32(Txthedef.Text))
+                {
+                    MessageBox.Show("Geçersiz Bir Hedef Girdiniz");
+                    Txthedef.Text = "0";
+                }
+            }
+            catch (Exception)
+            {
+                
+
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StreamWriter outR = new StreamWriter("C:/Users/ibokan/Desktop/outR.txt ");
+            outR.WriteLine(textBox1.Text);
+            outR.Close();
+
+            StreamWriter outQ = new StreamWriter("C:/Users/ibokan/Desktop/outQ.txt ");
+            outQ.WriteLine(textBox5.Text);
+            outQ.Close();
+
+            StreamWriter outPath = new StreamWriter("C:/Users/ibokan/Desktop/outPath.txt");
+            outPath.WriteLine(textBox2.Text);
+            outPath.Close();
+            MessageBox.Show("Veriler Masaüstüne Kayıt Edildi");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Graphics g;
+            Form2 frm2 = new Form2();
+            frm2.Show();
+            g = frm2.CreateGraphics();
+            Pen firca = new Pen(System.Drawing.Color.Black, 2);
+            Pen yolfirca = new Pen(System.Drawing.Color.Blue, 5);
+            int rw = listBox1.Items.Count;
+            double r2w=Math.Sqrt(rw);
+            int gen=0,yuk=0,sayac=0;
+            int xx=20, yy=20;
+            int[] find=new int[50];
+            int fp = 0;
+            SolidBrush Brush = new SolidBrush(Color.Red);
+            Font yazi = new Font("Georgia",12,FontStyle.Regular);
+            while(sayac<r2w)
+            {
+                for (int i = 0; i <r2w ; i++)
+                {      
+                    g.DrawRectangle(firca, xx + gen, yy + yuk, 50, 50);
+                    g.DrawString(fp.ToString(), yazi, Brush, xx + gen, yy + yuk);
+                    gen = gen + 50;
+                    fp++;
+                }
+                yuk = yuk + 50;
+                gen = 0;
+                sayac += 1;
+                }
+            int x1,x2,y1,y2;
+            for (int i = 0; i < yoldizi.Count-1 ; i++)
+            {
+                y1 = Convert.ToInt32(Math.Floor(Convert.ToInt32(yoldizi[i]) / r2w));
+                x1 = Convert.ToInt32(Convert.ToInt32(yoldizi[i]) % r2w);
+                y2 = Convert.ToInt32(Math.Floor(Convert.ToInt32(yoldizi[i + 1]) / r2w));
+                x2 = Convert.ToInt32(Convert.ToInt32(yoldizi[i + 1]) % r2w);
+                g.DrawLine(yolfirca,50+x1*50,50+y1*50,50+x2*50,50+y2*50);
+            }
+                
+            }
+
+        }
+
+    }
+
